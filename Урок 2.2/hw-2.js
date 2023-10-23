c32d764c8d34afb044c24e8e6a168639a0c2c091
Abadfari <wildsloth.screw@gmail.com>
2023-10-24 01:08:02 +0300

    Task 1 to 8 lesson 2.2

---
+++ Урок 2.2/hw-2.js
@@ -0,0 +1,57 @@
+/*
+let a = 10; // Задание 1
+alert(a);
+a = 20;
+alert(a);
+*/
+
+/*
+let firstIPhoneYear = 2007; // Задание 2
+alert(firstIPhoneYear);
+*/
+
+/*
+let creatorofJavaScript = "Brendan Eich"; // Задание 3
+alert(creatorofJavaScript)
+*/
+
+/*
+let num1 = 10; // Задание 4
+let num2 = 2;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+alert(sum);
+alert(difference);
+alert(product);
+alert(quotient);
+*/
+
+/*
+let result = (2 ** 5); // Задание 5
+alert(result);
+*/
+
+/*
+let a = 9; // Задание 6
+let b = 2;
+let result = a % b;
+alert(result);
+*/
+
+/*
+let num = 1; // Задание 7
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+*/
+
+let age = prompt("Сколько вам лет?"); // Задание 8
+alert("Ваш возраст " + age);
