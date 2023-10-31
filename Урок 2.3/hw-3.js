a76a03e317554d82e2203029f214cf4d104a4f8c
Abadfari <wildsloth.screw@gmail.com>
2023-10-31 21:07:21 +0300

    tasks 1 to 3

---
+++ Урок 2.3/hw-3.js
@@ -0,0 +1,38 @@
+/*
+let password = 'пароль'; // Задание 1
+
+let userInput = prompt('Введите пароль:');
+
+if (userInput === password) {
+    console.log('Пароль введен верно');
+} else {
+    console.log('Пароль введен неправильно');
+}
+*/
+/*
+let c = 5; // Задание 2
+
+if (c > 0 && c < 10) {
+    console.log('Верно')
+} else {
+    console.log('Неверно');
+}
+
+c = 0
+c > 0 && c < 10 // false
+c = 10
+c > 0 && c < 10 // false
+c = -3
+c > 0 && c < 10 // false
+c = 2
+c > 0 && c < 10 // true 
+
+let d = 168 // Задание 3;
+let e = 15;
+
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Неверно');
+}
+*/
