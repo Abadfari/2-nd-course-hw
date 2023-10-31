24cc70782fdb008effa05f3190f417f0d7ccd463
Abadfari <wildsloth.screw@gmail.com>
2023-10-31 21:25:12 +0300

    tasks 4 to 5

--- Урок 2.3/hw-3.js
+++ Урок 2.3/hw-3.js
@@ -35,4 +35,52 @@ if (d > 100 || e > 100) {
 } else {
     console.log('Неверно');
 }
+
+let a = '2'; // Задание 4
+let b = '3';
+// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
+alert(Number(a) + Number(b));
 */
+
+let monthNumber = prompt('Введите номер месяца');  // Задание 5
+
+switch (monthNumber) {
+    case '1':
+        console.log('Зима');
+        break;
+    case '2':
+        console.log('Зима');
+        break;
+    case '3':
+        console.log('Весна');
+        break;
+    case '4':
+        console.log('Весна');
+        break;
+    case '5':
+        console.log('Весна');
+        break;
+    case '6':
+        console.log('Лето');
+        break;
+    case '7':
+        console.log('Лето');
+        break;
+    case '8':
+        console.log('Лето');
+        break;
+    case '9':
+        console.log('Осень');
+        break;
+    case '10':
+        console.log('Осень');
+        break;
+    case '11':
+        console.log('Осень');
+        break;
+    case '12':
+        console.log('Зима');
+        break;
+    default: console.log('Такого месяца не существует!');
+        break;
+}
