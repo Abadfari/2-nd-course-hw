a82dad41782cea968839f785c600f2b9667f65d1
Abadfari <wildsloth.screw@gmail.com>
2023-12-10 00:06:07 +0300

    added a second game block and a game function

--- site.js
+++ site.js
@@ -10,4 +10,23 @@ function gameSeason(month) {
     } else {
         return "Неправильный номер месяца";
     }
-}
+};
+
+function wordGuessGame() {
+    const words = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+    const shuffledWords = words.sort(() => Math.random() - 0.5);
+    // Выводим перемешанный массив пользователю
+    alert(`${shuffledWords.join(', ')}`);
+    // Задаем вопросы пользователю
+    const firstGuess = prompt('Чему равнялся первый элемент массива?').toLowerCase();
+    const lastGuess = prompt('Чему равнялся последний элемент массива?').toLowerCase();
+
+    // Проверяем ответы пользователя
+    if (firstGuess === words[0].toLowerCase() && lastGuess === words[words.length - 1].toLowerCase()) {
+        alert('Поздравляем! Вы угадали оба элемента!');
+    } else if (firstGuess === words[0].toLowerCase() || lastGuess === words[words.length - 1].toLowerCase()) {
+        alert('Вы были близки к победе!');
+    } else {
+        return 'Вы ответили неверно. Попробуйте еще раз!';
+    }
+};
